c6c5cc4acf302f9c20e07c213f30525cf01d83da
Garry Yao <yaojun85@gmail.com>
2014-07-30 16:20:21 +0800

    use when.all to join promises

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
 var fs = require('fs');
 var when = require('when');
-var callbacks = require('when/callbacks');
-var lint = require('jsonlint');
+var nodefn = require('when/node');
+require('es6-shim');
 
 /**
  * Read file async and to parse the content as JSON,
- * invoke the first callback if succeed, invoke the second
- * callback if:
+ * return a promise when it succeed,
+ * and reject if:
  *  1. the file doesn't exist
  *  2. the file content is not valid JSON
  */
-function readJSON(file, success, failure) {
-	fs.readFile(file, {
+function readJSON(file) {
+	return nodefn.call(fs.readFile, file, {
 		encoding : 'utf8'
-	}, function (err, content) {
-		if (err) {
-			failure(err);
-		}
-		try {
-			success(JSON.parse(content));
-		} catch (er) {
-			failure(er);
-		}
+	}).then(function (content) {
+		return JSON.parse(content);
 	});
 }
 
-// use "readJSON", in case of an syntax error on rejection, lint the file contents
-var file_to_read = "./sample.json";
-callbacks.call(readJSON, file_to_read).then(console.log)
-.catch(SyntaxError, function handleSyntaxError(err) {
-	lint.parse(fs.readFileSync(file_to_read, {encoding : 'utf8'}));
+// read two json files and merge the result of them as one single object
+var p1 = readJSON("./sample.json");
+var p2 = readJSON("./sample2.json");
+
+// when.all([p1, p2])
+when.join(p1, p2).spread(function onBothSuccess(obj1, obj2) {
+	console.log(Object.assign(obj1, obj2));
 });
