4e9764bb08c9caa72c936c520217c1e3f138f31c
Garry Yao <yaojun85@gmail.com>
2014-07-30 15:06:46 +0800

    rewrite with promise

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 var fs = require('fs');
-
+var when = require('when');
 /**
- * Read file async and to parse the content as JSON
+ * Read file async and to parse the content as JSON,
+ * invoke the first callback if succeed, invoke the second
+ * callback if:
+ *  1. the file doesn't exist
+ *  2. the file content is not valid JSON
  */
-function readJSON(file, success, failure) {
+function readJSON(file) {
+	var df = when.defer();
 	fs.readFile(file, {
 		encoding : 'utf8'
-	}, function (err, content) {
-		if (err) {
-			failure(err);
+	}, function (er, content) {
+		if (er) {
+			df.reject(er);
 		}
 		try {
-			success(JSON.parse(content));
+			df.resolve(JSON.parse(content));
 		} catch (er) {
-			failure(er);
+			df.reject(er);
 		}
 	});
+	return df.promise;
 }
 
-// use "readJSON"
-readJSON("./sample.json", function onSuccess(obj) {
+// consume "readJSON" with promise
+readJSON("./sample.json").then(function onSuccess(obj) {
 	console.log(obj);
-}, function (err) {
+}).otherwise(function noFailure(err) {
 	console.error(err);
 });
